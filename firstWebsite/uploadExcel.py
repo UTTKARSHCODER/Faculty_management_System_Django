d853e7c467677c62941c73e159d3dffa91c75419
UTTKARSHCODER <uttkarshyadav702@gmail.com>
2026-01-02 12:07:04 +0530

    Removed unnecessary media files, segregated major function in different files to make code debug easier and look good, added unnecessary migration folder in .gitignore file to disallow it from being tracked and finally updated backend with strong validation at the time of form submissions.

---
+++ firstWebsite/uploadExcel.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from django.shortcuts import redirect, render
+from tablib import Dataset
+from django.contrib import messages
+from django.urls import reverse
+
+from firstWebsite.modals import Student_Directory, Faculty
+
+
+def upload_excel(request, pk):
+    if pk:
+        if request.method == 'POST':
+            dataset = Dataset()
+            new_data = request.FILES['excel_file']
+            if not new_data or new_data.name == '':
+                messages.error(request,'No file is selected or invalid file')
+                return "No file selected or invalid file", 400
+            imported_data = dataset.load(new_data.read(), format='xlsx')
+            df = pd.DataFrame(
+                imported_data.dict,
+                columns=imported_data.headers
+            )
+            if pk == 0:
+                df.columns = df.columns.str.lower().str.replace(' ', '_').str.strip()
+                for data in imported_data.dict:
+                    value = Student_Directory(name=data['name'],roll_no=data['roll_no'],college_id=data['college_id'],email=data['email'],student_phone_no=data['student_phone_no'],parent_phone_no=data['parent_phone_no'],address=data['address'])
+                    value.save()
+
+                messages.success(request,'We are glad to share that your excel file is uploaded successfully!')
+                return redirect(reverse('student-directory'))
+
+            elif pk == 1:
+                df.columns = df.columns.str.lower().str.replace(' ', '_').str
+                for data in df.to_dict(orient='records'):
+                    if not data['name']:
+                        continue
+
+                    value = Faculty(name=data['name'],emp_id=data['employee_id'],email=data['email'],department=data['department'],contact_number=data['contact_number'],status="NR")
+                    value.save()
+
+                return redirect(reverse('directory'))
+            else:
+                return render(request, '404.html')
+    return render(request,'404.html')
---
+++ firstWebsite/validations.py
@@ -0,0 +1,146 @@
+import os
+import re
+from django.contrib import messages
+
+def emailValidate(request,key):
+    if not key or key.strip() == '':
+        messages.error(request,"Email cannot be blank")
+        return False
+    elif not key.strip().endswith('@skit.ac.in'):
+        messages.error(request, 'Mail should always end with @skit.ac.in')
+        return False
+    else:
+        return True
+
+
+def mobileNumberValidate(request,key):
+    mobilepattern = r'[6789][0-9]{9}'
+    key = key.strip()
+    if not key or key == '':
+        error_message = f"Mobile Number cannot be empty"
+        messages.error(request, error_message)
+        return False
+    elif re.match(mobilepattern,key):
+        error_message = f"Mobile Number cannot contains special characters,digits,tabs or newline."
+        messages.error(request, error_message)
+        return False
+    else:
+        return True
+
+def validate_pan(pan_number):
+    # The Regex Pattern
+    # [A-Z]{3} -> First 3 alphabets
+    # [PCHFATBLJG] -> 4th character (Status)
+    # [A-Z] -> 5th character (Surname initial)
+    # [0-9]{4} -> Next 4 digits
+    # [A-Z] -> Last alphabet
+    pattern = r'^[A-Z]{3}[PCHFATBLJG][A-Z][0-9]{4}[A-Z]$'
+
+    # Ensure the input is treated as uppercase and stripped of whitespace
+    pan_number = str(pan_number).strip().upper()
+
+    if re.match(pattern, pan_number):
+        return True
+    else:
+        return False
+
+def nameValidate(request,key,tag):
+    namepattern = re.compile(r'[^a-zA-Z ]')
+    key = key.strip()
+    if not key or key == '':
+        error_message = f"{tag} cannot be empty"
+        messages.error(request,error_message)
+        return False
+    elif namepattern.search(key):
+        error_message = f"{tag} cannot contains special characters,digits,tabs or newline."
+        messages.error(request,error_message)
+        return False
+    else:
+        return True
+
+def alphanumnameValidate(request,key,tag):
+    key = key.strip()
+    namepattern = re.compile(r'[^a-zA-Z0-9 ]')
+    if not key or key == '':
+        error_message = f"{tag} cannot be empty"
+        messages.error(request, error_message)
+        return False
+    elif namepattern.search(key):
+        error_message = f"{tag} cannot contains special characters,digits,tabs or newline."
+        messages.error(request, error_message)
+        return False
+    else:
+        return True
+
+def numberValidate(request,key,tag):
+    if not key or key == 0:
+        error_message = f"{tag} cannot be empty"
+        messages.error(request,error_message)
+        return False
+    else:
+        return True
+
+def radiocheck(request,key,tag):
+    key = key.strip()
+    if len(key) < 1:
+        error_message = f"Please select at least one {tag}"
+        messages.error(request, error_message)
+        return False
+    else:
+        return True
+
+def fileValidate(request,key,tag):
+
+    limit_mb = 10
+    if key.size > limit_mb * 1024 * 1024:
+        messages.error(request, f"{tag} exceeded the file upload limit(10mb)")
+        return False
+
+    allowed_extension = ['.pdf']
+    extenstion = os.path.splitext(key.name)[1].lower()
+
+    if extenstion not in allowed_extension:
+        messages.error(request, f"{tag} should be of .pdf format")
+        return False
+
+    allowed_mime_types = ['application/pdf']
+    if key.content_type not in allowed_mime_types:
+        messages.error(request, f"{tag} should be of content type pdf")
+        return False
+
+    return True
+
+def addressValidate(request,key,tag):
+    pattern = re.compile(r'[^a-zA-Z0-9\s,./-]')
+    if not key or key.strip() == '':
+        error_message = f"{tag} cannot be empty"
+        messages.error(request, error_message)
+        return False
+    elif pattern.search(key.strip()):
+        result = pattern.search(key.strip())
+        error_message = f"{tag} cannot contain {result.group()}"
+        messages.error(request, error_message)
+        return False
+    else:
+        return True
+
+def imageFileValidate(request,key,tag):
+
+    limit_mb = 10
+    if key.size > limit_mb * 1024 * 1024:
+        messages.error(request, f"{tag} exceeded the file upload limit(10mb)")
+        return False
+
+    allowed_extension = ['.jpg','.jpeg','.png']
+    extenstion = os.path.splitext(key.name)[1].lower()
+
+    if extenstion not in allowed_extension:
+        messages.error(request, f"{tag} should be of .pdf format")
+        return False
+
+    allowed_mime_types = ['image/jpeg','image/png']
+    if key.content_type not in allowed_mime_types:
+        messages.error(request, f"{tag} should be of content type pdf")
+        return False
+
+    return True
