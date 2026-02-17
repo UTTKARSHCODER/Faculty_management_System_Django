ac8e25ecdf4463443e0e80fe6851caf681df2d23
Yogesh Singh <yogeshsingh777shekhawat@gmail.com>
2026-02-17 14:19:16 +0000

    added .pdf.png .jpeg for image and added na and NA for the link tab

--- static/script.js
+++ static/script.js
@@ -1128,8 +1128,8 @@ function generateSubForms() {
                                     <div class="row align-items-center">
                                         <label class="col-sm-4 col-form-label">Link to website of the Journal<span class="ms-1" style="color: red;">*</span><span class="fa-solid fa-circle-info" data-bs-toggle="tooltip" data-bs-placement="top" title="${fieldInfo['Link to website of the Journal']}"></span></label>
                                         <div class = "col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Enter DOI(Digital Object Identifier)" name = "lwj" required>
-                                            <div class="invalid-feedback">Please provide Link to website of the Journal starting with www., http://, or https://.</div>
+                                            <input type="text" class="form-control" placeholder="Enter Link to website of the Journal" name = "lwj" required>
+                                            <div class="invalid-feedback">Please provide Link to website of the Journal starting with www., http://, or https://. or write na</div>
                                         </div>
                                     </div>
                                 </div>
@@ -1252,7 +1252,7 @@ function generateSubForms() {
                                         <label class="col-sm-4 col-form-label">Upload Full Paper<span class="ms-1" style="color: red;">*</span><span class="fa-solid fa-circle-info" data-bs-toggle="tooltip" data-bs-placement="top" title="${fieldInfo['Upload Full Paper']}"></span>
                                         <h5 class="font-small mt-2" style="font-size: 14px; color: gray;">Upload .pdf,.jpg,.jpeg,.png File Format(MAX SIZE:- 2MB)</h5></label>
                                         <div class="col-sm-6">
-                                            <input type="file" class="form-control" accept=".pdf,.jpg,.jpeg,.png" name="proof_file" required>
+                                            <input type="file" class="form-control" accept=".pdf, image/jpeg, image/png" name="proof_file" required>
                                             <div class="invalid-feedback">Please upload a PDF file.</div>
                                         </div>
                                     </div>
@@ -1383,7 +1383,7 @@ function generateSubForms() {
                                         <label class="col-sm-4 col-form-label">Web Link<span class="ms-1" style="color: red;">*</span><span class="fa-solid fa-circle-info" data-bs-toggle="tooltip" data-bs-placement="top" title="${fieldInfo['Link to website of the Journal']}"></span></label>
                                         <div class = "col-sm-8">
                                             <input type="text" class="form-control" placeholder="Enter Link to website of the Journal" name = "lwj" required>
-                                            <div class="invalid-feedback">Please provide Link to website of the Journal starting with www., http://, or https://.</div>
+                                            <div class="invalid-feedback">Please provide Link to website of the Journal starting with www., http://, or https://. or write na</div>
                                         </div>
                                     </div>
                                 </div>
@@ -1437,7 +1437,7 @@ function generateSubForms() {
                                         <label class="col-sm-4 col-form-label">Upload Full Paper<span class="ms-1" style="color: red;">*</span><span class="fa-solid fa-circle-info" data-bs-toggle="tooltip" data-bs-placement="top" title="${fieldInfo['Upload Full Paper']}"></span>
                                         <h5 class="font-small mt-2" style="font-size: 14px; color: gray;">Upload .pdf,.jpg,.jpeg,.png File Format(MAX SIZE:- 2MB)</h5></label>
                                         <div class="col-sm-6">
-                                            <input type="file" class="form-control" accept=".pdf,.jpg,.jpeg,.png" name="proof_file" required>
+                                            <input type="file" class="form-control" accept=".pdf, image/jpeg, image/png" name="proof_file" required>
                                             <div class="invalid-feedback">Please upload a PDF file.</div>
                                         </div>
                                     </div>
@@ -1559,7 +1559,7 @@ function generateSubForms() {
                                         <label class="col-sm-4 col-form-label">Web Link<span class="ms-1" style="color: red;">*</span><span class="fa-solid fa-circle-info" data-bs-toggle="tooltip" data-bs-placement="top" title="${fieldInfo['Link to website of the Journal']}"></span></label>
                                         <div class = "col-sm-8">
                                             <input type="text" class="form-control" placeholder="Enter Link to website of the Journal" name = "lwj" required>
-                                            <div class="invalid-feedback">Please provide Link to website of the Journal starting with www., http://, or https://.</div>
+                                            <div class="invalid-feedback">Please provide Link to website of the Journal starting with www., http://, or https://. or write na</div>
                                         </div>
                                     </div>
                                 </div>
@@ -1613,7 +1613,7 @@ function generateSubForms() {
                                         <label class="col-sm-4 col-form-label">Upload Proof (Book Chapter/Front Page/Document etc.)<span class="ms-1" style="color: red;">*</span><span class="fa-solid fa-circle-info" data-bs-toggle="tooltip" data-bs-placement="top" title="${fieldInfo['Upload Full Paper']}"></span>
                                         <h5 class="font-small mt-2" style="font-size: 14px; color: gray;">Upload .pdf,.jpg,.jpeg,.png File Format(MAX SIZE:- 2MB)</h5></label>
                                         <div class="col-sm-6">
-                                            <input type="file" class="form-control" accept=".pdf,.jpg,.jpeg,.png" name="proof_file" required>
+                                            <input type="file" class="form-control" accept=".pdf, image/jpeg, image/png" name="proof_file" required>
                                             <div class="invalid-feedback">Please upload a PDF file.</div>
                                         </div>
                                     </div>
@@ -1771,7 +1771,7 @@ function generateSubForms() {
                                         <label class="col-sm-4 col-form-label">Link<span class="ms-1" style="color: red;">*</span><span class="fa-solid fa-circle-info" data-bs-toggle="tooltip" data-bs-placement="top" title="${fieldInfo['Link to website of the Journal']}"></span></label>
                                         <div class = "col-sm-8">
                                             <input type="text" class="form-control" pattern="^(https?:\/\/|www\.).+" placeholder="Enter Link to website of the Journal" name = "link" required>
-                                            <div class="invalid-feedback">Please provide a valid link starting with www., http://, or https://</div>
+                                            <div class="invalid-feedback">Please provide a valid link starting with www., http://, or https:// or write na</div>
                                         </div>
                                     </div>
                                 </div>
@@ -2112,11 +2112,11 @@ function generateSubForms() {
 function attachLinkValidation() {
     const linkFields = document.querySelectorAll('input[name="link"], input[name="lwj"], input[name="lrsj"], input[name="lap"]');
     // Updated pattern: REQUIRES either http://, https://, or www. at the start
-    const urlPattern = /^(https?:\/\/|www\.)[a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?(\.[a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?)*(\.[a-zA-Z]{2,})?([\/\?#].*)?$/;
+    const urlPattern = /^((https?:\/\/|www\.)[a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?(\\.[a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?)*(\\.\\[a-zA-Z]{2,})?([\/\?#].*)?$|^(na|NA)$)/;
 
     linkFields.forEach(field => {
         // Set HTML5 pattern attribute for native validation
-        field.setAttribute('pattern', '^(https?:\\/\\/|www\\.)[a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?(\\.[a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?)*(\\.\\[a-zA-Z]{2,})?([\\/?#].*)?$');
+        field.setAttribute('pattern', '^((https?:\/\/|www\.)[a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?(\\.[a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?)*(\\.\\[a-zA-Z]{2,})?([\/\?#].*)?$|^(na|NA)$)');
 
         field.addEventListener('blur', function() {
             validateLinkField(this, urlPattern);
