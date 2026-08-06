ab8a7bfe7d6f449e613eff2b982a05c8aeace174
Tarun Maheshwari <tarunsoni6102005@gmail.com>
2026-08-06 16:16:25 +0000

    Fixed the bug in hamburger menu

--- MyFirstDjangoWebsite/settings.py
+++ MyFirstDjangoWebsite/settings.py
@@ -79,6 +79,12 @@ MIDDLEWARE = [
     'firstWebsite.middleware.CustomAuthForceLogoutMiddleware'
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://localhost:8000',
+    'http://localhost:8000',
+    'https://*.app.github.dev',  # If using GitHub Codespaces
+]
+
 ROOT_URLCONF = 'MyFirstDjangoWebsite.urls'
 
 TEMPLATES = [
